3e1043fa95b967d24cddcb038315ecb807d90a4b
UlaşDK <udk97@hotmail.com>
2022-08-11 12:35:20 +0300

    Basic TicTacToe

---
+++ src/component/Card.js
@@ -0,0 +1,50 @@
+import React, { useState } from 'react'
+import "./Card.css"
+
+const Card = () => {
+
+    const [turn,setTurn] = useState("X")
+    const [cell,setCell] = useState(Array(9).fill(""))
+
+    const handleClikc = (num)=>{
+        let square = [...cell]
+        if(turn === "X"){
+            square[num] = "X"
+            setTurn("O")
+        }else{
+            square[num] = "O"
+            setTurn("X")
+        }
+        setCell(square);
+    }
+
+    const Fill = ({num})=>{
+        return <td onClick={() => handleClikc(num)}>{cell[num]}</td>;
+    }
+  return (
+      <div className="container">
+        <table>
+          Turn of : {turn}
+          <tbody>
+            <tr>
+              <Fill num={1} />
+              <Fill num={2} />
+              <Fill num={3} />
+            </tr>
+            <tr>
+              <Fill num={4} />
+              <Fill num={5} />
+              <Fill num={6} />
+            </tr>
+            <tr>
+              <Fill num={7} />
+              <Fill num={8} />
+              <Fill num={9} />
+            </tr>
+          </tbody>
+        </table>
+      </div>
+  );
+}
+
+export default Card
