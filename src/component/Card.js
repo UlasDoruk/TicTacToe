657c39d131968cfb1e992debab4862023d4b26ae
UlaşDK <udk97@hotmail.com>
2022-08-11 13:34:48 +0300

    End of Dynamics

--- src/component/Card.js
+++ src/component/Card.js
@@ -5,6 +5,36 @@ const Card = () => {
 
     const [turn,setTurn] = useState("X")
     const [cell,setCell] = useState(Array(9).fill(""))
+    const [winner,setWinner] = useState()
+
+    const checkWinner = (square)=>{
+        const combos = {
+            across :[
+                [0,1,2],
+                [3,4,5],
+                [6,7,8],
+            ],
+            down : [
+                [0,3,6],
+                [1,4,7],
+                [2,5,8],
+            ],
+            diagonal : [
+                [0,4,8],
+                [2,4,6],
+            ]}
+            for (let i in combos){
+                combos[i].forEach(element => {
+                    if(square[element[0]]===""|| square[element[1]]===""|| square[element[2]]===""){
+                        //do nothing 
+                    }else if (square[element[0]]=== square[element[1]] && square[element[1]]=== square[element[2]]){
+                        setWinner(square[element[0]]);
+                    }
+                });
+            }
+        }
+
+
 
     const handleClikc = (num)=>{
         if(cell[num] !== ""){
@@ -19,35 +49,43 @@ const Card = () => {
             square[num] = "O"
             setTurn("X")
         }
+        checkWinner(square)
         setCell(square);
+    }
+
+    const handleReset = ()=>{
+        setWinner(null)
+        setCell(Array(9).fill(""))
     }
 
     const Fill = ({num})=>{
         return <td onClick={() => handleClikc(num)}>{cell[num]}</td>;
     }
   return (
-      <div className="container">
-        <table>
-          Turn of : {turn}
-          <tbody>
-            <tr>
-              <Fill num={0} />
-              <Fill num={1} />
-              <Fill num={2} />
-            </tr>
-            <tr>
-              <Fill num={3} />
-              <Fill num={4} />
-              <Fill num={5} />
-            </tr>
-            <tr>
-              <Fill num={6} />
-              <Fill num={7} />
-              <Fill num={8} />
-            </tr>
-          </tbody>
-        </table>
-      </div>
+    <div className="container">
+      <table>
+        {<h4>Turn : {turn}</h4>}
+        <tbody>
+          <tr>
+            <Fill num={0} />
+            <Fill num={1} />
+            <Fill num={2} />
+          </tr>
+          <tr>
+            <Fill num={3} />
+            <Fill num={4} />
+            <Fill num={5} />
+          </tr>
+          <tr>
+            <Fill num={6} />
+            <Fill num={7} />
+            <Fill num={8} />
+          </tr>
+        </tbody>
+      </table>
+      {winner && <h2>Winner is : {winner}</h2>}
+      <button className='btn' onClick={() => handleReset()}>Play again</button>
+    </div>
   );
 }
 
